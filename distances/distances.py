70ccf72e535c6ccde95b191a1275c727fdbcc4d3
Carlos-A-Ramos-G <aramosguzmanc@gmail.com>
2022-09-26 16:23:37 +0200

    Update distances.py

--- distances/distances.py
+++ distances/distances.py
@@ -3,7 +3,9 @@ import pandas as pd
 import mdtraj as md
 import matplotlib.pyplot as plt
 
-t = md.load('traj.dcd', top='parm.prmtop')
+def traj_load():
+    return md.load('traj.dcd', top='parm.prmtop')
+
 #pair of atoms in distances numbering from zero
 pairs = [[[80,2780]], 
         [[82, 2781]], 
@@ -12,12 +14,14 @@ pairs = [[[80,2780]],
 #compute the distances between pairs
 def distance_measure(pairs):
     all_distances = []
+    t = traj_load()
     for pair in pairs:
         all_distances.append(md.compute_distances(t, pair)*10)
     
     
     #Save the distances to a file
     f = open('distances.dat', 'w')
+    # it is up to you the distances naming
     f.write('distance1   distance2   distance3\n')
     for i in range(t.n_frames):
         for j in range(len(all_distances)):
@@ -26,15 +30,15 @@ def distance_measure(pairs):
     f.close()
 
 def distance_plotter(file, pairs):
-    cols = [f'Dist {i}' for i in range(1, len(pairs)+1)]
-    df = pd.read_csv(file, names=cols, delim_whitespace=True, skiprows=1)
-    for col in cols:
-        plt.plot(df[col])
+    df = pd.read_csv(file, delim_whitespace=True)
+    distances = df.columns
+    for distance in distances:
+        plt.plot(df[distance])
     plt.title('distances along simulation')
-    plt.legend(cols)
+    plt.legend(distances)
     plt.xlabel('time')
     plt.ylabel('$\AA$')
     plt.show()
 
-#distance_measure(pairs)
+distance_measure(pairs)
 distance_plotter('distances.dat', pairs)
